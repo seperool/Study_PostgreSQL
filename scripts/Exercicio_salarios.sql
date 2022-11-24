d34f5d688ce90f57a9a2779da7ffd5a7fd144313
Sergio Pedro <seperool@gmail.com>
2022-11-23 21:11:47 -0300

    Exercicios de estatística aplicado ao postgreSQL

---
+++ scripts/Exercicio_salarios.sql
@@ -0,0 +1,21 @@
+-- database: data_science
+-- table: funcionarios
+
+/* Exercicios */
+
+-- verificando tabela
+SELECT * FROM funcionarios;
+
+/* 1 - Qual a moda dos salários? A moda dos salários diz algo de relevante? */
+
+/* 2 - Qual a moda departamental? (Qual o departamento que mais emprega?) */
+
+/* 3 - Qual o desvio-padrão de cada departamento? */
+
+/* 4 - Cálcule a mediana salarial de todo set de dados. */
+
+/* 5 - Qual é a amplitude de todos os salários? */
+
+/* 6 - Cálcule as principais medidas estatísticas por departamento. */
+
+/* 7 - Qual departamento te dá maior oportunidade de ganhar mais? --Outdoor*/
